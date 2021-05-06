d5a958676e1476d0cf5ea3dbe6296e3438c06b14
gql <guoqianliang@foxmail.com>
2021-05-06 08:10:01 +0800

    后台系统完成

--- vue-admin-template-master/src/views/hosp/list.vue
+++ vue-admin-template-master/src/views/hosp/list.vue
@@ -76,6 +76,12 @@
 
       <el-table-column label="操作" width="230" align="center">
         <template slot-scope="scope">
+          <router-link :to="'/hospSet/hospital/show/' + scope.row.id">
+            <el-button type="primary" size="mini">查看</el-button>
+          </router-link>
+          <router-link :to="'/hospSet/hospital/schedule/' + scope.row.hoscode">
+            <el-button type="primary" size="mini">排班</el-button>
+          </router-link>
           <el-button
             v-if="scope.row.status == 1"
             type="primary"
